b59d9fce5bbc07ca8c038c66689f2c8bdb9fb321
Franci G <franci@handels.com>
2021-02-15 15:10:21 -0800

    added onClick Event handler & React hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  // useState() results:
+  //a) state object (getter)
+  // b) updater function (setter)
+  const [counter, setCounter] = useState(0);
   return (
+    //onClick = {functionRef}
     <div className="App">
-      <button>CLICK HERE</button>
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
     </div>
   );
 }
